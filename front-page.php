59a2c3d092a8ad632874a5544d2cd22c1158ac81
annashpb <anna.sh.pb@gmail.com>
2020-02-28 15:00:13 +0200

    linked showrooms to the front page

--- front-page.php
+++ front-page.php
@@ -20,12 +20,22 @@
 
         <section class="showrooms-main">
             <div class="container">
-                <ul class="showrooms-main__list">
-                    <li class="showrooms-main__item showroom-main">
-                        <p class="showroom-main__name">Showroom name</p>
-                        <a href="#" class="showroom-main__link">Go to the showroom</a>
-                    </li>
-                </ul>
+                <?php
+                $args = array(
+                    'post_type' => 'showroom',
+                );
+                $the_query = new WP_Query($args); ?>
+                <?php if ($the_query->have_posts()) : ?>
+                    <ul class="showrooms-main__list">
+                        <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+                            <li class="showrooms-main__item showroom-main">
+                                <p class="showroom-main__name"><?php echo get_the_title(); ?></p>
+                                <a href="#" class="showroom-main__link">Go to the showroom</a>
+                            </li>
+                        <?php endwhile; ?>
+                        <?php wp_reset_postdata(); ?>                  
+                    </ul>
+                <?php endif; ?>
             </div>
         </section>
 
