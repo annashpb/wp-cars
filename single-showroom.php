c0a9c6e9f5f31be577f6c00bf0d2a4b5c3822b5d
annashpb <anna.sh.pb@gmail.com>
2020-03-03 14:29:37 +0200

    view the cars of a specific showroom

--- single-showroom.php
+++ single-showroom.php
@@ -37,20 +37,24 @@
     $args = array(
         'post_type' => 'car',
     );
-    $the_query = new WP_Query($args); ?>
+    $the_query = new WP_Query($args);
+    $srId = get_the_ID(); ?>
 
-<pre>
-<?php var_dump($the_query); ?>
-</pre>
+
 
     <?php if ($the_query->have_posts()) : ?>
         <ul class="cars__list">
             <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+                <?php
+                $showroom = get_field('showroom');
+                if ($showroom->ID === $srId) :
+                ?>
                 <li class="cars__item car">
-                    <a href="<?php the_permalink(); ?>" class="car__link">
-                        <p class="car__name"><?php echo get_the_title(); ?></p>
-                    </a>
-                </li>
+                        <a href="<?php the_permalink(); ?>" class="car__link">
+                            <p class="car__name"><?php echo get_the_title(); ?></p>
+                        </a>
+                    </li>
+                <?php endif; ?>
             <?php endwhile; ?>
             <?php wp_reset_postdata(); ?>
         </ul>
